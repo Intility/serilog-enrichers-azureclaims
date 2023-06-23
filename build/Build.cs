7dca34a97d0fae65a55e3ff5c822d98d8d65d89d
Thorstein Løkensgard <thorstein.lokensgard@intility.no>
2023-06-23 09:54:01 +0200

    ci: test with nuke

--- build/Build.cs
+++ build/Build.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
@@ -6,6 +7,7 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
@@ -19,10 +21,13 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.Test);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    AbsolutePath TestProjectDir => RootDirectory / "tests/Serilog.Enrichers.AzureAuthInfo.Tests";
+    AbsolutePath TestProjectFile => TestProjectDir / "Serilog.Enrichers.AzureAuthInfo.Tests.csproj";
 
     Target Clean => _ => _
         .Before(Restore)
@@ -41,4 +46,15 @@ class Build : NukeBuild
         {
         });
 
+
+    Target Test => _ => _
+    .DependsOn(Compile)
+    .Executes(() =>
+    {
+        DotNetTasks.DotNetTest(s => s
+           .SetProjectFile(TestProjectFile)
+           .SetConfiguration(Configuration)
+           .SetNoBuild(InvokedTargets.Contains(Compile))
+           .EnableNoRestore());
+    });
 }
