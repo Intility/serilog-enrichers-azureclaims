c0f9bbad0cefa0dfd77001eaaf6f4fa9c2b76f16
Thorstein Løkensgard <thorstein.lokensgard@intility.no>
2023-06-30 12:46:51 +0200

    ci: add codecov

---
+++ build/Build.TestCoverage.cs
@@ -0,0 +1,50 @@
+﻿using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Codecov;
+using Nuke.Common.Tools.Coverlet;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.ReportGenerator;
+
+[GitHubActions("Build and Test",
+           GitHubActionsImage.UbuntuLatest,
+           OnPushBranches = new[] { "main" },
+           OnPullRequestBranches = new[] { "main" },
+           InvokedTargets = new[] { nameof(Test) },
+           FetchDepth = 0,
+    AutoGenerate = true)]
+partial class Build : NukeBuild
+{
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Produces(TestResultDirectory / "*.trx")
+        .Produces(TestResultDirectory / "*.xml")
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetTest(s => s
+               .SetProjectFile(TestProjectFile)
+               .SetConfiguration(Configuration)
+               .SetNoBuild(InvokedTargets.Contains(Compile))
+               .EnableNoRestore()
+               .ResetVerbosity()
+               .SetResultsDirectory(TestResultDirectory)
+               .EnableCollectCoverage()
+               .SetCoverletOutputFormat(CoverletOutputFormat.opencover)
+               .SetProcessArgumentConfigurator(a => a.Add("--collect:\"XPlat Code Coverage\""))
+               .SetLoggers($"junit;LogFileName={"coverage.trx"}")
+               .SetCoverletOutput(TestResultDirectory / "coverage.cobertura.xml")
+               );
+        });
+
+    Target ReportCoverage => _ => _
+        .DependsOn(Test)
+        .Consumes(Test)
+    .Executes(() =>
+    {
+        ReportGeneratorTasks.ReportGenerator(s => s
+           .SetReports(TestResultDirectory / "**/*.xml")
+           .SetReportTypes(ReportTypes.Cobertura)
+           .SetTargetDirectory(CoverageReportDirectory)
+           .SetAssemblyFilters("-*Tests"));
+    });
+}
--- build/Build.cs
+++ build/Build.cs
@@ -1,23 +1,9 @@
 using Nuke.Common;
 using Nuke.Common.CI;
-using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.IO;
-using Nuke.Common.Tooling;
-using Nuke.Common.Tools.Coverlet;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
-using Nuke.Common.Tools.ReportGenerator;
-using System;
-using System.Linq;
-using static Nuke.Common.IO.PathConstruction;
 
-[GitHubActions("Build and Test",
-           GitHubActionsImage.UbuntuLatest,
-           OnPushBranches = new[] { "main" },
-           OnPullRequestBranches = new[] { "main" },
-           InvokedTargets = new[] { nameof(Test) },
-           FetchDepth = 0,
-    AutoGenerate = true)]
 [ShutdownDotNetAfterServerBuild]
 partial class Build : NukeBuild
 {
@@ -57,38 +43,4 @@ partial class Build : NukeBuild
                 .SetInformationalVersion(GitVersion.InformationalVersion)
                 .EnableNoRestore());
         });
-
-
-    Target Test => _ => _
-        .DependsOn(Compile)
-        .Produces(TestResultDirectory / "*.trx")
-        .Produces(TestResultDirectory / "*.xml")
-        .Executes(() =>
-        {
-            DotNetTasks.DotNetTest(s => s
-               .SetProjectFile(TestProjectFile)
-               .SetConfiguration(Configuration)
-               .SetNoBuild(InvokedTargets.Contains(Compile))
-               .EnableNoRestore()
-               .ResetVerbosity()
-               .SetResultsDirectory(TestResultDirectory)
-               .EnableCollectCoverage()
-               .SetCoverletOutputFormat(CoverletOutputFormat.opencover)
-               .SetProcessArgumentConfigurator(a => a.Add("--collect:\"XPlat Code Coverage\""))
-               .SetLoggers($"junit;LogFileName={"coverage.trx"}")
-               .SetCoverletOutput(TestResultDirectory / "coverage.cobertura.xml")
-               );
-        });
-
-    Target ReportCoverage => _ => _
-        .DependsOn(Test)
-        .Consumes(Test)
-    .Executes(() =>
-    {
-        ReportGeneratorTasks.ReportGenerator(s => s
-           .SetReports(TestResultDirectory / "**/*.xml")
-           .SetReportTypes(ReportTypes.Cobertura, ReportTypes.TextSummary)
-           .SetTargetDirectory(CoverageReportDirectory)
-           .SetAssemblyFilters("-*Tests"));
-    });
 }
